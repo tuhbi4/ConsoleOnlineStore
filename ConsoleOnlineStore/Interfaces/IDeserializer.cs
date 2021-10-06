f715a8995d049845d5fbb699759b2467149c04ee
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 01:38:09 +0300

    add interface IDeserializer

---
+++ ConsoleOnlineStore/Interfaces/IDeserializer.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+
+namespace ConsoleOnlineStore.Interfaces
+{
+    public interface IDeserializer
+    {
+        public List<Product> GetDataFromJson(string fileName);
+    }
+}
