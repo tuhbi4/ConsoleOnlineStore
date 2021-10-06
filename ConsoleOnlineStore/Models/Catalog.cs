9e7b9e4d69fb3d907016e7d503774202b23fb167
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 01:37:58 +0300

    add class Catalog

---
+++ ConsoleOnlineStore/Models/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using System.Collections.Generic;
+
+namespace ConsoleOnlineStore.Models
+{
+    public class Catalog : ICatalog
+    {
+        public List<Product> ProductList { get; }
+        private readonly Deserializer deserializer = new();
+        public Catalog()
+        {
+            ProductList = deserializer.GetDataFromJson(@"database.json");
+        }
+    }
+}
