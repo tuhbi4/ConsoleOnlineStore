31a950636a43a3b8d52a9483306eca7691f58910
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 01:37:36 +0300

    add class Product

---
+++ ConsoleOnlineStore/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleOnlineStore.Interfaces;
+
+namespace ConsoleOnlineStore.Models
+{
+    public class Product : IProduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
