8cfa21daa2709b1a73e247e26901db870a162052
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 01:51:45 +0300

    add interface ISerializer

---
+++ ConsoleOnlineStore/Interfaces/ISerializer.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+
+namespace ConsoleOnlineStore.Interfaces
+{
+    public interface ISerializer
+    {
+        public void SaveDataToJson(string fileName, List<Product> productList);
+    }
+}
