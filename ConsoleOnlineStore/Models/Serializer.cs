2ef4c538297b7847cf8e54e7379a18cb94ace68a
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 01:51:57 +0300

    add class Serializer

---
+++ ConsoleOnlineStore/Models/Serializer.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleOnlineStore.Models
+{
+    public class Serializer : ISerializer
+    {
+        public void SaveDataToJson(string fileName, List<Product> productList)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            foreach (Product product in productList)
+            {
+                string jsonString = JsonSerializer.Serialize<Product>(product, options);
+                File.AppendAllText(fileName, jsonString + "\r\n");
+            }
+        }
+    }
+}
