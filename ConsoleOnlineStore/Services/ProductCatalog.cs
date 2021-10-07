ee27fb0054a581fbfb8ca0e7c42a6bfee3ed7309
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 13:02:42 +0300

    fix conversations

--- ConsoleOnlineStore/Services/ProductCatalog.cs
+++ ConsoleOnlineStore/Services/ProductCatalog.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
 using ConsoleOnlineStore.Interfaces;
+using ConsoleOnlineStore.Models;
 
-namespace ConsoleOnlineStore.Models
+namespace ConsoleOnlineStore.Services
 {
-    public class Catalog : ICatalog
+    public class ProductCatalog : IProductCatalog
     {
         public List<Product> ProductList { get; }
         private readonly JsonDeserializer deserializer = new();
 
-        public Catalog()
+        public ProductCatalog()
         {
             ProductList = deserializer.GetData(@"database.json");
         }
