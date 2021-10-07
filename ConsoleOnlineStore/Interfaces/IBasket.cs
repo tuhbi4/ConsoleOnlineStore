34eb1354a22659c7f7a06dfc723cbd04fe1c4127
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 08:36:34 +0300

    add interface IBasket

---
+++ ConsoleOnlineStore/Interfaces/IBasket.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+
+namespace ConsoleOnlineStore.Interfaces
+{
+    public interface IBasket
+    {
+        public List<Product> ProductList { get; }
+
+        public void AddProductToBasket(Product product);
+
+        public void ClearBasket();
+
+        public void CompletePurchase();
+    }
+}
