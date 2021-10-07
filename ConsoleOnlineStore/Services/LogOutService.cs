9be49e169cf806f170dbe00e8ec50778d6ab1fe7
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 07:02:53 +0300

    add class LogOutService

---
+++ ConsoleOnlineStore/Services/LogOutService.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ConsoleOnlineStore.Services
+{
+    public class LogOutService : ILogOutService
+    {
+        private readonly Deserializer deserializer = new();
+        private readonly string filepath = ConfigurationManager.AppSettings.Get("accountsDatabase");
+
+        public void LogOut(string login)
+        {
+            List<Account> accountList = deserializer.GetAccountListFromJson(filepath);
+            Account accountForMatching = accountList.Find(x => x.Login.Contains(login));
+            accountForMatching.LogOut();
+        }
+    }
+}
