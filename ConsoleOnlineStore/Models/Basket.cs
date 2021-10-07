54008607b7787b71b79cda7a5371cc837d6c2ad9
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 08:37:45 +0300

    add class Basket

---
+++ ConsoleOnlineStore/Models/Basket.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+
+namespace ConsoleOnlineStore.Models
+{
+    public class Basket : IBasket
+    {
+        public List<Product> ProductList { get; }
+        private readonly Deserializer deserializer = new();
+        private readonly Serializer serializer = new();
+        private readonly string basketFilepath = ConfigurationManager.AppSettings.Get("basketDatabase");
+        private readonly string timerFilepath = ConfigurationManager.AppSettings.Get("timerconfig");
+
+        public Basket()
+        {
+            ProductList = new();
+        }
+
+        public void AddProductToBasket(Product product)
+        {
+            ProductList.Add(product);
+            if (ProductList.Count == 1)
+            {
+                int dueTime = deserializer.GetTimerFromJson(timerFilepath);
+                TimerCallback callback = new(ClearBasket);
+                using Timer timer = new(callback, null, dueTime, Timeout.Infinite);
+            }
+        }
+
+        public void ClearBasket()
+        {
+            ProductList.Clear();
+        }
+
+        public void ClearBasket(object obj)
+        {
+            ProductList.Clear();
+        }
+
+        public void CompletePurchase()
+        {
+            serializer.SaveProductListToJson(basketFilepath, ProductList);
+        }
+    }
+}
