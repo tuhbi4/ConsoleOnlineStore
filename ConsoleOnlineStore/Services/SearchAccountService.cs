fc6d14fe34af9b7503cf4715b1ab59b4d4d84fdf
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 07:04:15 +0300

    add class SearchAccountService

---
+++ ConsoleOnlineStore/Services/SearchAccountService.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ConsoleOnlineStore.Services
+{
+    public class SearchAccountService : ISearchAccountService
+    {
+        private readonly Deserializer deserializer = new();
+        private readonly string filepath = ConfigurationManager.AppSettings.Get("accountsDatabase");
+
+        public bool IsExist(string login)
+        {
+            List<Account> accountList = deserializer.GetAccountListFromJson(filepath);
+            return accountList.Exists(item => item.Login == login);
+        }
+    }
+}
