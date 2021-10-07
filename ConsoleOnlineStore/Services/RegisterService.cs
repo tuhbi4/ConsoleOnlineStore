dbd572ee013d57cb58ee211939b54c31e32646b6
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 07:03:22 +0300

    add class RegisterService

---
+++ ConsoleOnlineStore/Services/RegisterService.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using ConsoleOnlineStore.Models;
+using System.Configuration;
+
+namespace ConsoleOnlineStore.Services
+{
+    public class RegisterService : IRegisterService
+    {
+        private readonly Serializer serializer = new();
+        private readonly string filepath = ConfigurationManager.AppSettings.Get("accountsDatabase");
+
+        public void Register(string login, string password)
+        {
+            Account newAccount = new(login, password);
+            serializer.SaveNewAccountToJson(filepath, newAccount);
+        }
+    }
+}
