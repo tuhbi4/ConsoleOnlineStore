1db4978c2fb402af40b9b980195de27c06d7cc12
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-07 07:03:54 +0300

    add class MatchPasswordService

---
+++ ConsoleOnlineStore/Services/MatchPasswordService.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleOnlineStore.Interfaces;
+using ConsoleOnlineStore.Models;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ConsoleOnlineStore.Services
+{
+    public class MatchPasswordService : IMatchPasswordService
+    {
+        private readonly Deserializer deserializer = new();
+        private readonly string filepath = ConfigurationManager.AppSettings.Get("accountsDatabase");
+
+        public bool IsMatched(string login, string password)
+        {
+            List<Account> accountList = deserializer.GetAccountListFromJson(filepath);
+            Account accountForMatching = accountList.Find(x => x.Login.Contains(login));
+            return accountForMatching.Password == password;
+        }
+    }
+}
