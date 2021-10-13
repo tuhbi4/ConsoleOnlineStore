ce04ed8d2805e897c1cc3a454b2e38615cff00c3
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-13 19:08:06 +0300

    add services

---
+++ OnlineStoreView/Services/ValidatorServices/RegistrationFormValidatorService.cs
@@ -0,0 +1,42 @@
+﻿using OnlineStoreView.Interfaces;
+
+namespace OnlineStoreView.Services
+{
+    public class RegistrationFormValidatorService : IValidatorService
+    {
+        public virtual bool IsValidAnswer(int input, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        public virtual bool IsValidAnswer(string input, out string errorMessage)
+        {
+            if (input.Length == 0)
+            {
+                errorMessage = "This field must be filled.";
+                return false;
+            }
+
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        public virtual bool IsValidAnswer(string login, string password, out string errorMessage)
+        {
+            if (login.Length == 0)
+            {
+                errorMessage = "Login must be specified.";
+                return false;
+            }
+            if (password.Length == 0)
+            {
+                errorMessage = "Password must be specified.";
+                return false;
+            }
+
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
