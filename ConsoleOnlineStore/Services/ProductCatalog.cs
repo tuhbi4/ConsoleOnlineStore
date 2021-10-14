aee1901b42a9d3737f7fa96a65e9eff7b2b288d4
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-14 21:27:07 +0300

    fix refactor

--- ConsoleOnlineStore/Services/ProductCatalog.cs
+++ ConsoleOnlineStore/Services/ProductCatalog.cs
@@ -6,31 +6,11 @@ namespace ConsoleOnlineStore.Services
 {
     public class ProductCatalog : IProductCatalog
     {
-        public List<CatalogItem> CatalogList { get; }
-        private readonly List<Product> productList;
+        public List<Product> CatalogList { get; }
 
         public ProductCatalog(IRepository<Product> productRepository)
         {
-            productList = productRepository.Read();
-            MakeCatalog();
-        }
-
-        private void MakeCatalog()
-        {
-            foreach (Product product in productList)
-            {
-                foreach (CatalogItem catalogItem in CatalogList)
-                {
-                    if (CatalogList.Count != 0 && catalogItem.Id == product.Id)
-                    {
-                        catalogItem.Increment();
-                    }
-                    else
-                    {
-                        CatalogList.Add(new(product));
-                    }
-                }
-            }
+            CatalogList = productRepository.Read();
         }
     }
 }
