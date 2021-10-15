9b257e615a22686fd9276aa9f3ea47deafb2a4db
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-10-15 20:23:12 +0300

    add view models

---
+++ OnlineStoreView/ViewModels/ResponseForms/AuthorizationFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using OnlineStoreView.Interfaces;
+
+namespace OnlineStoreView.ViewModels
+{
+    public class AuthorizationFormViewModel : IMenu
+    {
+        public List<string> Menu { get; } = new List<string>
+        {
+            "Enter login",
+            "Enter password",
+        };
+    }
+}
